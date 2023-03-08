5f6ec5283f56fae544bec21fd37c3cf21ea089f2
Mirian Santana <miriansantana93@gmail.com>
2023-03-08 12:46:23 -0300

    Added init on class screenOne and button back

--- ProjectFashion/ProjectFashion/ScreenOneViewController.swift
+++ ProjectFashion/ProjectFashion/ScreenOneViewController.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class ScreenOneViewController: UIViewController {
     
+    @IBOutlet weak var imageFashion: UIImageView!
+    @IBOutlet weak var lblText: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    
     var screenOne = FashionStyles(title: "", image: "", text: "", style: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageFashion.image = UIImage(named: screenOne.image)
+        lblText.text = screenOne.text
+        lblTitle.text = screenOne.title
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btBack(_ sender: Any) {
+        
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
+    
 }
